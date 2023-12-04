b6ead906f3bae68e0e67fb8b185fc7a0d9f05905
Dwayne Fernandes <dwayne.ferns16@gmail.com>
2023-12-04 17:47:58 +0000

    Initialised the repo

---
+++ hashmap.h
@@ -0,0 +1,4 @@
+template<typename K, typename V>
+class Hashmap
+{
+};
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "hashmapl.h"
+
+
+int main() 
+{
+
+    std::cout << "First program" << std::endl;
+
+    return 0;
+}
